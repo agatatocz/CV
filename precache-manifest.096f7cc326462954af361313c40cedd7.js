69f32741a17cf2bb3c1536cb29df87cbfe49ba2a
Agata <agatatocz@gmail.com>
2019-02-12 23:28:01 +0100

    Updates

---
+++ precache-manifest.096f7cc326462954af361313c40cedd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cv/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "c0a2b9ce3141ea19ade7",
+    "url": "/cv/static/css/main.54e23697.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cv/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f831e5916a5f75a72198",
+    "url": "/cv/static/js/1.f831e591.chunk.js"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "c0a2b9ce3141ea19ade7",
+    "url": "/cv/static/js/main.c0a2b9ce.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cv/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cv/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cv/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "f831e5916a5f75a72198",
+    "url": "/cv/static/css/1.cd03608d.chunk.css"
+  },
+  {
+    "revision": "47cad3ae37a1c90b6ee82ed13ebf8218",
+    "url": "/cv/index.html"
+  }
+];
